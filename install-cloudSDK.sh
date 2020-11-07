7bb0792887e6c8d88a9e40328079609b6832783d
rakeshmadivi <44462799+rakeshmadivi@users.noreply.github.com>
2020-11-07 15:59:51 +0530

    Create install-cloudSDK.sh

---
+++ install-cloudSDK.sh
@@ -0,0 +1,14 @@
+# Add the Cloud SDK distribution URI as a package source:
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+
+sudo apt-get install apt-transport-https ca-certificates gnupg
+
+# Import the Google Cloud public key:
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+
+# Update and install the Cloud SDK:
+sudo apt-get update && sudo apt-get install google-cloud-sdk
+
+# Docker Tip: If installing the Cloud SDK inside a Docker image, use a single RUN step instead:
+RUN echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list && curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg  add - && apt-get update -y && apt-get install google-cloud-sdk -y
+      
